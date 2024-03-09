9db08c82ceebc2c5f7f3668ff744cd303825378e
Imren More <98141213+imrenmore@users.noreply.github.com>
2024-03-09 22:58:25 +0000

    SO-65 -Created Responsive Input fields for text inputs for first name, last name, email and password

---
+++ src/components/Account/Inputfield/Inputfield.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+interface AccountInputFieldProps {
+	label: string;
+	placeholder: string;
+	type: 'text' | 'email' | 'password';
+	value: any;
+	setValue: (value: any) => void;
+}
+
+const InputField = ({
+	label,
+	placeholder,
+	type,
+	value: valTmp,
+	setValue: setVal,
+}: AccountInputFieldProps) => {
+	const [value, setValue] = useState(valTmp);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+		setVal(e.target.value);
+	};
+
+	return (
+		<div className="infocolumns">
+			<label
+				htmlFor={label}
+				className="labelsformat"
+			>
+				{label}
+			</label>
+			<input
+				type={type}
+				id={label}
+				className="inputf"
+				placeholder={placeholder}
+				value={value}
+				onChange={handleChange}
+				required
+			/>
+		</div>
+	);
+};
+
+export default InputField;
