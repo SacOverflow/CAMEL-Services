bfb54e77488e3260c751ec868261b4b31beae95e
Junior <49306278+jvniorrr@users.noreply.github.com>
2024-04-08 01:58:32 -0700

    SO-123
    - Navigation bar content rendering of notifications
    - Signup and Login redirections
    - Searchbar functions Case 1 & 2 in `globalsearch.test.tsx`
    - Home page content fixes & test cases
    - Footer test cases integrated & passing

--- __tests__/routes/Footer.test.tsx
+++ __tests__/routes/Footer.test.tsx
@@ -1,20 +1,50 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
-//import Footer from '@src/components/SharedComponents/Footer';
-
-import {Footer} from '../../src/components/SharedComponents/Footer'
+import { Footer } from '../../src/components/SharedComponents/Footer';
 describe('Footer component', () => {
-  it('Dispalys footer to the correct website when clicked', () => {
-    const href = 'http://localhost:3000/';
-    render(<Footer></Footer>);
+	it('Displays footer to has developers name rendered', () => {
+		const href = 'http://localhost:3000/';
+		render(<Footer></Footer>);
 
-    //const link = screen.getByText('https://github.com/jvniorrr');
+		expect(screen.getByText('Hashem Jaber')).toBeInTheDocument();
+	});
 
-    //expect(link).toBeInTheDocument();
+	it('displays and redirects to the correct website when clicked', () => {
+		const { container, getByText } = render(<Footer />);
 
-    //userEvent.click(link);
-    expect(screen.getByText("Hashem Jaber")).toBeInTheDocument();
-    //expect(window.location.href).toBe(href);
-  });
+		const developerNames = [
+			'Fernando Mendoza',
+			'Hashem Jaber',
+			'Jacob Correa',
+			'Joseph Doan',
+			'Miguel Lopez',
+			'Imren More',
+			'Kiranjot Kaur',
+			'Dakota Conn',
+		];
+
+		const developerLinks = [
+			'https://github.com/jvniorrr',
+			'https://github.com/hashemJaber',
+			'https://github.com/RealHoltz',
+			'https://github.com/JDoan03',
+			'https://github.com/Miguel1357',
+			'https://github.com/imrenmore',
+			'https://github.com/KiranKaur3',
+			'https://github.com/DGConn',
+		];
+
+		// check if the developer names are correct
+		let developerEls = developerNames.map(name => getByText(name));
+		developerEls.forEach(el => {
+			expect(el).toBeInTheDocument();
+
+			// check if the developer links are correct
+			let link =
+				developerLinks[
+					developerNames.indexOf(el.textContent as string)
+				];
+			expect(el.closest('a')).toHaveAttribute('href', link);
+		});
+	});
 });
